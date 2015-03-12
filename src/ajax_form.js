8a3971c867b75973dffba0511de548385ec3dd49
solomreb <solomreb@onid.oregonstate.edu>
2015-03-12 15:16:26 -0700

    test.html/verify.php successfully reject invalid input and check for username

---
+++ src/ajax_form.js
@@ -0,0 +1,67 @@
+
+function CreateRequestObj () {
+        // although IE supports the XMLHttpRequest object, but it does not work on local files.
+    var forceActiveX = (window.ActiveXObject && location.protocol === "file:");
+    if (window.XMLHttpRequest && !forceActiveX) {
+        return new XMLHttpRequest();
+    }
+    else {
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        } catch(e) {}
+    }
+}
+
+    // create HTTP request body form form data
+function GetMessageBody (form) {
+    var data = "";
+    for (var i = 0; i < form.elements.length; i++) {
+        var elem = form.elements[i];
+        if (elem.name) {
+            var nodeName = elem.nodeName.toLowerCase ();
+            var type = elem.type ? elem.type.toLowerCase () : "";
+
+                // if an input:checked or input:radio is not checked, skip it
+            if (nodeName === "input" && (type === "checkbox" || type === "radio")) {
+                if (!elem.checked) {
+                    continue;
+                }
+            }
+
+            var param = "";
+                // select element is special, if no value is specified the text must be sent
+            if (nodeName === "select") {
+                for (var j = 0; j < elem.options.length; j++) {
+                    var option = elem.options[j];
+                    if (option.selected) {
+                        var valueAttr = option.getAttributeNode ("value");
+                        var value = (valueAttr && valueAttr.specified) ? option.value : option.text;
+                        if (param != "") {
+                            param += "&";
+                        }
+                        param += encodeURIComponent (elem.name) + "=" + encodeURIComponent (value);
+                    }
+                }
+            }
+            else {
+                param = encodeURIComponent (elem.name) + "=" + encodeURIComponent (elem.value);
+            }
+
+            if (data != "") {
+                data += "&";
+            }
+            data += param;                  
+        }
+    }
+    return data;
+}
+
+    // returns whether the HTTP request was successful
+function IsRequestSuccessful (httpRequest) {
+        // IE: sometimes 1223 instead of 204
+    var success = (httpRequest.status == 0 || 
+        (httpRequest.status >= 200 && httpRequest.status < 300) || 
+        httpRequest.status == 304 || httpRequest.status == 1223);
+    
+    return success;
+}
--- src/verify.php
+++ src/verify.php
@@ -6,30 +6,63 @@ if (!$mysqli || $mysqli->connect_errno){
     echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
 }
 
-$username = trim($_POST["username"]);
+define("ERR_INVALID_USERNAME", "1");
+define("ERR_EXISTING_USERNAME", "2");
+define("ERR_INVALID_PASSWORD", "3");
+define("ERR_DIFFERENT_PASSWORDS", "4");
 
-//check if username is blank
-if ($username == "") {
-	echo "Username must be filled in"; 
+$errors = array ();
+
+if (isset ($_POST["userName"])) {
+    $username = $_POST["userName"];
+    $len = strlen ($username);
+    if ($len < 2 || $len > 20) {
+        array_push ($errors, ERR_INVALID_USERNAME);
+    }
+    else {
+
+    //if username already exists, send error message
+		$query = "select username from users where username = ? ";
+		$stmt = $mysqli->prepare($query);
+		$stmt->bind_param('s',$username);
+		$stmt->execute();
+		$stmt->store_result();
+		$numRows = $stmt->num_rows;
+		if($stmt->num_rows>0){
+			array_push ($errors, ERR_EXISTING_USERNAME);
+		}	  
+    }
 }
-
-//if not blank, check the database for that username
 else {
-
-	$query = "select username from users where username = ? ";
-	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param('s',$username);
-	$stmt->execute();
-	$stmt->store_result();
-	$numRows = $stmt->num_rows;
-
- 	if($numRows > 0)
-		echo "Sorry, username \"".$username."\" is already in use";
-	else
-		echo "Username \"".$username."\" is available";
+    array_push ($errors, ERR_INVALID_USERNAME);
 }
 
+if (isset ($_POST["password"])) {
+    $password = $_POST["password"];
+    $len = strlen ($password);
+    if ($len < 6 || $len > 20) {
+        array_push ($errors, ERR_INVALID_PASSWORD);
+    }
+    else {
+        if (!isset ($_POST["repassword"]) || strcmp ($password, $_POST["repassword"]) != 0) {
+            array_push ($errors, ERR_DIFFERENT_PASSWORDS);
+        }
+    }
+}
+else {
+    array_push ($errors, ERR_INVALID_PASSWORD);
+}
 
-	
+$response = "";
+if (sizeof ($errors) > 0) {
+    $response = implode (",", $errors);
+}
+else {
+        // some db operations, save username and password ...
+    
+    $response = "ok";
+}
+
+echo ($response);	
 
 ?>
