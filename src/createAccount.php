dae061e9dafeb924444ce3bd4f2f66738733f4d7
solomreb <solomreb@onid.oregonstate.edu>
2015-03-12 22:21:26 -0700

    createAccount works

--- src/createAccount.php
+++ src/createAccount.php
@@ -1,3 +1,4 @@
+
 <?php
 include 'storedInfo.php';
 //header('Location: index.php');
@@ -6,20 +7,19 @@ $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "solomreb-db", $myPassword,"s
 if (!$mysqli || $mysqli->connect_errno){
     echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
     }
-    
+$fname = $_POST["fname"]; 
+$lname = $_POST["lname"];    
+$phone = $_POST["phone"];    
+$email = $_POST["email"];       
 $username = $_POST["userName"];  
 $pwd = $_POST["password"];
 
-echo $username;
-echo $pwd;
 
-$sqlQuery= "INSERT INTO  `solomreb-db`.`users` VALUES ( ?, ?);";	
+$sqlQuery= "INSERT INTO  `solomreb-db`.`walkers`(fname, lname, phone, email, username, pwd) VALUES (?, ?, ?, ?, ?, ?);";	
 $stmt = $mysqli->prepare($sqlQuery);
-$stmt->bind_param('ss',$username, $pwd);
-
-
+$stmt->bind_param('ssssss', $fname, $lname, $phone, $email, $username, $pwd);
 //execute prepared statement
 $stmt->execute();
-printf("%d Row inserted.\n", $stmt->affected_rows);
 
+printf("%d Row inserted.\n", $stmt->affected_rows);
 ?>
