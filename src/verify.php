dd8da8cbf029e48b1b176c503f2d629087da9c82
solomreb <solomreb@onid.oregonstate.edu>
2015-03-12 22:02:46 -0700

    register.html/verify.php work

--- src/verify.php
+++ src/verify.php
@@ -1,17 +1,48 @@
-<?php 
-include 'storedInfo.php';
+<?php
 
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "solomreb-db", $myPassword,"solomreb-db");
-if (!$mysqli || $mysqli->connect_errno){
-    echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
 
-define("ERR_INVALID_USERNAME", "1");
-define("ERR_EXISTING_USERNAME", "2");
-define("ERR_INVALID_PASSWORD", "3");
-define("ERR_DIFFERENT_PASSWORDS", "4");
+
+define("ERR_INVALID_FNAME", "1");
+define("ERR_INVALID_LNAME", "2");
+define("ERR_INVALID_PHONE", "3");
+define("ERR_INVALID_EMAIL", "4");
+define("ERR_INVALID_USERNAME", "5");
+define("ERR_EXISTING_USERNAME", "6");
+define("ERR_INVALID_PASSWORD", "7");
+define("ERR_DIFFERENT_PASSWORDS", "8");
+
 
 $errors = array ();
+if (isset ($_POST["fname"])) {
+    $fname = $_POST["fname"];
+    $len = strlen ($fname);
+    if ($len < 1) {
+        array_push ($errors, ERR_INVALID_FNAME);
+    }
+}
+if (isset ($_POST["lname"])) {
+    $lname = $_POST["lname"];
+    $len = strlen ($lname);
+    if ($len < 1) {
+        array_push ($errors, ERR_INVALID_LNAME);
+    }
+}
+if (isset ($_POST["phone"])) {
+    $phone = $_POST["phone"];
+    $len = strlen ($phone);
+    if((preg_match("/[^0-9]/", '', $phone)) && strlen($phone) == 10){
+        array_push ($errors, ERR_INVALID_PHONE);
+    }
+}
+
+if (isset ($_POST["email"])) {
+    $email = $_POST["email"];
+    $len = strlen ($email);
+    if ($len < 7 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push ($errors, ERR_INVALID_EMAIL);
+    }
+}
+
 
 if (isset ($_POST["userName"])) {
     $username = $_POST["userName"];
@@ -20,17 +51,10 @@ if (isset ($_POST["userName"])) {
         array_push ($errors, ERR_INVALID_USERNAME);
     }
     else {
-
-    //if username already exists, send error message
-		$query = "select username from users where username = ? ";
-		$stmt = $mysqli->prepare($query);
-		$stmt->bind_param('s',$username);
-		$stmt->execute();
-		$stmt->store_result();
-		$numRows = $stmt->num_rows;
-		if($stmt->num_rows>0){
-			array_push ($errors, ERR_EXISTING_USERNAME);
-		}	  
+            // check the name of registered users
+        if (strcasecmp ($username, "Dottoro") == 0) {
+            array_push ($errors, ERR_EXISTING_USERNAME);
+        }
     }
 }
 else {
@@ -62,7 +86,8 @@ else {
     
     $response = "ok";
 }
+    // 2 secs delay
+sleep (2);
 
-echo ($response);	
-
+echo ($response);
 ?>
